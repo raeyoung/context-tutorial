c2db8b96c2950b9e4892f04e8b46ebb6cc99a219
raeyoung <raeyoung233@gmail.com>
2021-01-23 23:10:09 +0900

    Using static contextType

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ColorConsumer from '../contexts/color'
+import React, { Component } from 'react';
+import ColorContext from '../contexts/color'
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-const SelectColors = () => {
-  return (
-    <div>
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = color => {
+    this.context.action.setColor(color);
+  }
+  handleSetSubcolor = subcolor => {
+    this.context.action.setSubColor(subcolor);
+  }
+  render() {
+    return (
+      <div>
       <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
       {({ actions }) => (
       <div style={{ display: 'flex' }}>
         {colors.map(color => (
@@ -16,19 +24,19 @@ const SelectColors = () => {
             style={{
               background: color, width: '24px', height: '24px', cursor: 'pointer'
             }}
-            onClick={() => actions.setColor(color)}
-            onContextMenu={e => {
+            onClick = {() => this.handleSetColor(color)}
+            onContextMenu = {e => {
               e.preventDefault();
-              actions.setSubcolor(color);
+              this.handleSetSubcolor(color);
             }}
           />
         ))}
       </div>
       )}
-      </ColorConsumer>
       <hr />
     </div>
-  )
+    )
+  }
 }
 
 export default SelectColors
